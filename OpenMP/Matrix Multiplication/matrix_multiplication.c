ba4fb2307a8fd35d58594b2421d85c293ba69ee7
John Fantell <fantej@rpi.edu>
2020-02-20 12:32:03 -0500

    Backing up latest

--- OpenMP/Matrix Multiplication/matrix_multiplication.c
+++ OpenMP/Matrix Multiplication/matrix_multiplication.c
@@ -1,16 +1,21 @@
-// OpenMP header 
 #include <omp.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
 
-void matrix_mult(int r, int c, float **A, float **B, float **C){
+//Matrix Multiplication using OpenMP
+//	Algorithm overview: https://en.wikipedia.org/wiki/Matrix_multiplication_algorithm
+//Compile instructions:
+//	gcc -o matrixmult matrix_multiplication.c -fopenmp
+//Run instructions:
+// ./matrixmult
+void matrix_mult(int n, int m, int p, float **A, float **B, float **C){
 
 	int i, j, k;
 	#pragma omp parallel shared(A,B,C) private(i,j,k)
 	#pragma omp for simd
-	for(i = 0; i < r; ++i) {
-		for(j = 0; j < c ; j++){
-			for(k=0; k < c ; k++){
+	for(i = 0; i < n; ++i) {
+		for(j = 0; j < p ; j++){
+			for(k=0; k < m ; k++){
 				C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
 			}
 		}
@@ -40,30 +45,37 @@ float ** initialize_matrix(int r, int c, char type){
   
 int main(int argc, char* argv[]) 
 {
-	 
+	//Record duration for each experiment
 	double start; 
 	double end; 
     double elapsed_time;
     double cum_elapsed_time, avg_elapsed_time;
-    int i, j;
-    int n = 10; //Number of trials to run per experiment
+
+ 	//Number of experiments to run for each thread count
+    int n = 10;
+
+    //Number of thread counts to test
+    int threads[7] = {1, 2, 4, 8, 16, 32, 64};
 	
-	int r = 100, c = 100;
+	//Dimensions of matrices
+	int n = 100, m = 100, p = 100;
 	float **A;
 	float **B;
 	float **C;
 	
-	A = initialize_matrix(r,c,'A');
-	B = initialize_matrix(r,c,'B');
-	C = initialize_matrix(r,c,'C');
+	//Initialize 3 matrices
+	A = initialize_matrix(n,m,'A');
+	B = initialize_matrix(m,p,'B');
+	C = initialize_matrix(n,p,'C');
 
-	int threads[7] = {1, 2, 4, 8, 16, 32, 64};
-	for(i=0; i < 7; i++){
+	//Multiply matrices
+	//Record average elapsed time for each thread count
+	for(int i=0; i < 7; i++){
 		cum_elapsed_time = 0;
 		omp_set_num_threads(threads[i]);
-		for(j=0; j<n; j++){ //Three experiments for each number of threads
+		for(int j=0; j<n; j++){
 			start = omp_get_wtime();
-			matrix_mult(r, c, A, B, C);
+			matrix_mult(n, m, p, A, B, C);
 			end = omp_get_wtime();
 			elapsed_time = end-start;
 			cum_elapsed_time += elapsed_time;
@@ -73,12 +85,12 @@ int main(int argc, char* argv[])
 	}
 
 	//Free A
-	for (i=0; i<r; i++) 
+	for (int i=0; i<r; i++) 
 		 free(A[i]);
 	free(A);
 
 	//Free B
-	for (i=0; i<r; i++) 
+	for (int i=0; i<r; i++) 
 		 free(B[i]);
 	free(B);
 	
